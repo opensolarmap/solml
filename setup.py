db6fa86e0667abfa6b509c5ec0ddb78a760edc5a
Michel Blancard <michel.blancard@data.gouv.fr>
2017-02-09 15:36:43 +0100

    make a package

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='solml',
+    version='0.0.1',
+    description='OpenSolarMap - machine learning',
+    long_description=long_description,
+    url='https://github.com/opensolarmap/solml',
+    author='Michel Blancard',
+    author_email='michel.blancard@data.gouv.fr',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['solml'],
+    install_requires=[
+        'jupyter>=1',
+        'pandas>=0.19',
+        'numpy>=1.12',
+        'matplotlib>=2',
+        'GDAL>=2',
+        'Pillow>=4',
+    ],
+)
